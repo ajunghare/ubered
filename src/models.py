0d0bf589aeffe939059e920572512b0e25b9bcb7
Ajit Junghare <ajunghare123@gmail.com>
2020-01-05 21:42:20 +0530

    initial commit

---
+++ src/models.py
@@ -0,0 +1,7 @@
+class Address:
+    lat = "0.0"
+    long = "0.0"
+
+    def __init__(self, lat, long):
+        self.lat = lat
+        self.long = long
---
+++ src/test.py
@@ -0,0 +1,2 @@
+def met(source, destination, time_of_day, email):
+    pass
